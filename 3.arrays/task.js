0ae7673813d0a879cf36002edd24cc2b4cf901f8
KatFad <q46q46@mail.ru>
2022-07-13 14:19:48 +0300

    'выполнение_ДЗ_массивы'

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,15 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  function ArrIndex (elem,index) {
+    if ( arr1[index] === arr2[index] ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  result = arr1.every(ArrIndex) && arr2.every(ArrIndex);
 
   return result; // boolean
 }
@@ -10,6 +19,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter(item => item >0).filter(item => item % 3 ===0).map(item => item*10);
 
   return resultArr; // array
 }
