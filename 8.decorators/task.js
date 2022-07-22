138718a2ed74465fa3c3f86344d49725339a77d0
KatFad <q46q46@mail.ru>
2022-07-22 12:45:11 +0300

    'выполнение_ДЗ_декор'

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,64 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+    let cache = [];
+    function wrapper(...args) {
+        const hash = args.join(','); // получаем правильный хэш
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+            if (objectInCache) { // если элемент найден
+                console.log('Из кэша: ' + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение
+                return 'Из кэша: ' + objectInCache.value;
+            }
+            let result = func(...args); // в кэше результата нет - придётся считать
+            cache.push({hash: hash , value: result}) ; // добавляем элемент с правильной структурой
+            if (cache.length > 5) {
+            cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый)
+            }
+            console.log('Вычисляем: ' + result);
+            return 'Вычисляем: ' + result;
+        }
+        return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func,ms) {
   // Ваш код
+        let timerId;
+        let firstTime = true;
+
+        return function (...args) {
+            clearTimeout(timerId);
+            if (firstTime) {
+                func.apply(this, args);
+                firstTime = false;
+            }
+           else {
+                timerId = setTimeout(() => {
+                    func.apply(this, args);
+                }, ms);
+            }
+        }
+
 }
 
 function debounceDecorator2(func) {
   // Ваш код
+    let timerId;
+    let firstTime = true;
+
+    return function wrapper(...args) {
+        clearTimeout(timerId);
+        wrapper.count++;
+        if (firstTime) {
+            func.apply(this, args);
+            firstTime = false;
+        }
+        else {
+            timerId = setTimeout(() => {
+                func.apply(this, args);
+            }, ms);
+        }
+    }
+
+    wrapper.count = 0;
+    return wrapper
 }
